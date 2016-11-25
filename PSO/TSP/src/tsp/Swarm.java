95d6f4634a8325c43447642509829e912f466c5d
DESKTOP-6LVSRE6\Daniel <danmurilloac@Gmail.com>
2016-11-25 11:11:38 -0700

    Cosas de la clase SWARM mas el archivo de constantes

--- PSO/TSP/src/tsp/Swarm.java
+++ PSO/TSP/src/tsp/Swarm.java
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 package tsp;
+import static tsp.Constants.N_CIUDADES;
 
 /**
  *
  * @author Carlos Antonio
  */
 public class Swarm {
+    int[] gBest;
+    int apGBest;
+    int c1,c2,iter;
+    double wMin,wMax,w;
+    Particle[] particulas;
+    
+    public Swarm(){
+        apGBest = 999999;
+        c1 = 999999;
+        c2 = 0;
+        particulas = new Particle[N_CIUDADES];
+    }
+    
     
 }
