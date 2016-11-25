07b5fed3fb9af2f8df5bb8e5431bf6fb80228927
Topitzin <murder.t20@gmail.com>
2016-11-25 11:22:41 -0700

    Begin of particle class

--- PSO/TSP/src/tsp/Particle.java
+++ PSO/TSP/src/tsp/Particle.java
@@ -10,5 +10,20 @@ package tsp;
  * @author Carlos Antonio
  */
 public class Particle {
-    
+    private int[] pBest;
+    private int[] path;
+    private int aptitude;
+    private int velocidad;
+
+    //constructor de la particula
+    public void Particle(){ 
+        pBest = new int[48];
+        path = new int[48];
+        aptitude = 0;
+        velocidad = 0;
+    }
+    //public void Particle(){ }
+    public double calcAptitud(){ return 0;}
+    public double calcVelocidad(int c1, int c2, int[] gBest){ return 0; }
+    public void actuPath(){ }
 }
